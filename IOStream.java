51c36468d1d1f93735193b8086f1b92a744118f2
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-05 10:22:58 +0200

    used Scanner to get input from console and fullfil the requirements

---
+++ IOStream.java
@@ -0,0 +1,39 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class IOStream {
+
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fw = null; // output channel or stream
+        String s;
+        try {
+            fw = new FileWriter("output.txt"); // output.txt is created and it where my streams data will be placed
+            PrintWriter pw = new PrintWriter(fw); // filtered output stream or channel
+
+            // getting user input from console (readline)
+            Scanner in = new Scanner(System.in);
+
+            int countLine = 0;
+            System.out.println("Enter input: ");
+            s = in.nextLine();
+            while (!s.isEmpty()) {
+                countLine++;
+                s = countLine + ": " + s;
+                System.out.println(s);
+                pw.println(s);
+                s = in.nextLine();
+            }
+            if (s.isEmpty()) {
+                System.out.println("Closed application...");
+            }
+            pw.flush();
+            pw.close();
+            fw.close();
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+}
